1a26f3ffff3692d32d806f9d8bb2891c255eee7d
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-06-04 21:33:39 +0700

    SeasonDisplay App Done!

--- section-4-to-6/seasons/src/index.js
+++ section-4-to-6/seasons/src/index.js
@@ -1,8 +1,37 @@
-import React from "react";
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  return <div>Hi there</div>
+class App extends React.Component {
+  state = { lat: null, errorMessage: '' };
+
+  getPosition = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  };
+
+  componentDidMount() {
+    console.log('My conponent was rendered to the screen');
+    this.getPosition();
+  }
+
+  componentDidUpdate() {
+    console.log('My component was just updated');
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading!</div>;
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,4 +40,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
