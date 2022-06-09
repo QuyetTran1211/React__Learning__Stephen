3d0c829f9f6f5a2ddc6be207e2559480d5f60494
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-06-09 16:18:03 +0700

    First commit Dropdown app

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Accordion from './compponents/Accordion';
-import Search from './compponents/Search';
+// import Search from './compponents/Search';
+import Dropdown from './compponents/Dropdown';
 
 // const items = [
 //   {
@@ -17,12 +18,34 @@ import Search from './compponents/Search';
 //   }
 // ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+];
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
