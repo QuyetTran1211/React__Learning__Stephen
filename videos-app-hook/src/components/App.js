1e9df73545cb6f69f10477b2a5a97db04c45d4de
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-06-11 13:08:49 +0700

    implement reuse hook

--- videos-app-hook/src/components/App.js
+++ videos-app-hook/src/components/App.js
@@ -1,42 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar.js';
-import youtube from '../api/youtube.js';
+
 import VideoList from './VideoList.js';
 import VideoDetails from './VideoDetails.js';
+import useVideos from '../hooks/useVideos.js';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
-
+  const [videos, search] = useVideos('tree');
   useEffect(() => {
-    onInputSubmit('tree');
-  }, []);
-
-  const onInputSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term
-      }
-    });
-
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
-
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
-      <SearchBar onSubmit={onInputSubmit} />
+      <SearchBar onSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
             <VideoDetails video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
